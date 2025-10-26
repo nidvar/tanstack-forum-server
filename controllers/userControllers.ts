f59917c38d30e5c5afa17bd2b4824fb5d5e2e521
nidvar <djayshine87@hotmail.com>
2025-10-26 17:38:13 +1100

    register and login error messages setup. register email and username check done

--- controllers/userControllers.ts
+++ controllers/userControllers.ts
@@ -4,12 +4,21 @@ import User from '../models/User';
 
 import bcrypt from "bcryptjs";
 
-export const checkPassword = function(password:String){
-    console.log(password);
-}
-
 export const register = async function(req: Request, res: Response){
     try{
+
+        const existingUser = await Promise.all([
+            User.findOne({ email: req.body.email }),
+            User.findOne({ username: req.body.username })
+        ]);
+
+        if(existingUser[0]){
+            return res.status(409).json({ error: "Email already in use" });
+        }
+        if(existingUser[1]){
+            return res.status(409).json({ error: "Username already in use" });
+        }
+
         const password = req.body.password;
         const hash = await bcrypt.hash(password, 10);
 
@@ -17,19 +26,31 @@ export const register = async function(req: Request, res: Response){
             username: req.body.username,
             email: req.body.email,
             password: hash
-        }
+        };
 
-        User.create(user);
+        await User.create(user);
+        return res.json({ message: 'User registered successfully' });
     }catch(error){
         console.log('register error ===>', error);
-        return res.json(error)
+        return res.status(500).json({ error: 'Server error' });
     }
 };
 
 export const login = async function(req: Request, res: Response){
     try{
-        console.log('body ====================== ',req.body);
+        const user = await User.findOne({ email: req.body.email });
+        if(user){
+            const ok = await bcrypt.compare(req.body.password, user.password);
+            if(ok){
+                return res.json({message:'login successful!'});
+            }else{
+                return res.status(409).json({error:'Incorrect username or password'});
+            }
+        }else{
+            return res.status(409).json({error:'Incorrect username or password'});
+        }
     }catch(error){
         console.log('login error ===>', error);
+        return res.status(500).json({ error: 'Server error' });
     }
 }
--- routes/postRoutes.ts
+++ routes/postRoutes.ts
@@ -4,13 +4,9 @@ const router = express.Router();
 import {allPosts, singlePost, createSinglePost, deleteSinglePost, updateSinglePost} from '../controllers/postControllers'
 
 router.get('/', allPosts);
-
 router.get('/:id', singlePost);
-
 router.post('/', createSinglePost);
-
 router.delete('/:id', deleteSinglePost);
-
 router.put('/:id', updateSinglePost);
 
 export default router;
